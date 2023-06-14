a8fb7d87ab455cd518fff2e957a11be4857618bd
Adrien <adriensauvage59@gmail.com>
2023-06-14 16:35:09 +0200

    added housing page w/ title&location

---
+++ src/pages/housing/index.jsx
@@ -0,0 +1,27 @@
+import Error from '../../components/error';
+import HousingDatas from '../../assets/data/logements.json';
+import './style.scss';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+function Housing() {
+    const { id } = useParams();
+    const data = HousingDatas.find(data => data.id === id);
+    if (!data) {
+        return <Error />
+    }
+    return (
+        <main>
+            <article className='housingDetails'>
+                <div className='apartmentDetails'>
+                    <h2 className="apartmentTitle">{data.title}</h2>
+                    <p className="apartmentLocation">{data.location}</p>
+                    <div className="housingTags">
+                    </div>
+                </div>
+            </article>
+        </main>
+    )
+}
+
+export default Housing
