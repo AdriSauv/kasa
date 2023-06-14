62ec154ae28b73e5b7eb6dea3b322091d8935721
Adrien <adriensauvage59@gmail.com>
2023-06-14 10:18:02 +0200

    added navbar header + style + responsive

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo-main.png';
 import "./style.css";
@@ -6,6 +7,10 @@ function Header(){
     return (
         <header className='header'>
             <Link to="/"><img src={logo} className='logo' alt='Logo Kasa' /></Link>
+            <nav className='navbar'>
+            <NavLink to="/" className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "" }>Accueil</NavLink>
+            <NavLink to="/about" className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "" }>A propos</NavLink>
+            </nav>
         </header>
     )
 };
